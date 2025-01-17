b43b970f0b9c8ceb471b502bfc52aef5099aee99
oschultz47 <oschultz47@tamu.edu>
2025-01-16 22:16:48 -0600

    add student homepage route

---
+++ app/app/controllers/student_home_controller.rb
@@ -0,0 +1,4 @@
+class StudentHomeController < ApplicationController
+  def index
+  end
+end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "student_home#index"
+  get "student_home/index", to: "student_home#index", as: "student_home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
