62d15ed9e1ebceeda6c056dd7494e684d0eafbec
caseykung17 <caseykung17@tamu.edu>
2025-01-28 18:08:03 -0600

    added rspec tests

--- app/app/controllers/users_controller.rb
+++ app/app/controllers/users_controller.rb
@@ -4,13 +4,14 @@ class UsersController < ApplicationController
   end
 
   def save_instructor
-    instructor = User.find(params[:instructor_id])
-    
-    if current_user.update(instructor_id: instructor.id)
-      flash[:notice] = "Instructor saved successfully!"
-    else
-      flash[:alert] = "Failed to save instructor."
-    end
+    begin
+      instructor = User.find(params[:instructor_id])
+      if current_user.update(instructor_id: instructor.id)
+        flash[:notice] = "Instructor saved successfully!"
+      end
+      rescue ActiveRecord::RecordNotFound
+        flash[:alert] = "Failed to save instructor."
+      end
   
     redirect_back(fallback_location: user_path(current_user.id)) # Redirects back to the same page
   end
