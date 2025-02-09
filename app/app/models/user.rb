14ebf123a084d1a2078ee75af17ed2b1dfcfa519
Leo Gonzalez <legona00@gmail.com>
2025-02-08 18:53:00 -0600

    Added Rspec test for changes to user model

--- app/app/models/user.rb
+++ app/app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   enum :role, { student: 0, instructor: 1 }
 
-  def full_name
-    "#{first_name} #{last_name}"
-  end
-
   belongs_to :instructor, class_name: "User", optional: true
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
+
+  def full_name
+    
+    "#{first_name} #{last_name}"
+  end
 end
---
+++ app/spec/models/user_spec.rb
@@ -0,0 +1,64 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  let(:user) {User.create!(first_name: "Johnny", last_name: "Manziel", email: "test@tamu.edu")}
+  let(:instructor) {User.create!(first_name: "Paul", last_name: "Taele", email: "prof@tamu.edu", role: "instructor")}
+  let(:question) {Question.create!(topic_id: 1, type_id: 1, template_text: "TEST QUESTION")}
+
+  describe "valid information" do
+    it "is valid" do
+      expect(user).to be_valid
+    end
+
+    it "is invalid without a first name" do
+      user.first_name = nil;
+      expect(user).not_to be_valid
+    end
+
+    it "is invalid without a last name" do
+      user.last_name = nil;
+      expect(user).not_to be_valid
+    end
+
+    it "is invalid without an email" do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+
+    it "is invalid with a duplicate email" do
+      user.email = "test@tamu.edu"
+      new_student = User.create(first_name: "John", last_name: "Doe", email: "test@tamu.edu")
+      expect(new_student).to_not be_valid
+    end
+
+    it "defaults role to student" do
+      expect(user.role).to eq("student")
+    end
+  end
+
+  describe "table associations" do
+    it { should have_many(:submissions).dependent(:destroy)}
+    it { should have_many(:questions).through(:submissions)}
+    it { should belong_to(:instructor).optional}
+  end
+
+  describe "#full_name" do
+    it "returns full name correctly" do
+      expect(user.full_name).to eq("Johnny Manziel")
+    end
+  end
+
+  describe "#update_user_submissions" do
+    it "increments total submissions" do
+      expect(user.update_user_submissions(false)).to change(user.total_submissions).by(1)
+    end
+
+    it "increments correct submissions when correct" do
+      expect(user.update_user_submissions(true)).to change(user.correct_submissions).by(1)
+    end
+
+    it "does not increment correct submissions when wrong" do
+      expect(user.update_user_submissions(false)).not_to change(user.correct_submissions)
+    end
+  end
+end
