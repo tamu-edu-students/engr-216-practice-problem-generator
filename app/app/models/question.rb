937e089e35c708af854ac6df4267e5ff8ae4cb79
Leo Gonzalez <legona00@gmail.com>
2025-02-08 20:09:05 -0600

    Question and Submission model passing all tests

--- app/app/models/question.rb
+++ app/app/models/question.rb
@@ -4,4 +4,8 @@ class Question < ApplicationRecord
 
   has_many :submissions, dependent: :destroy
   has_many :users, through: :submissions
+
+  validates :topic_id, :type_id, :template_text, presence: true
+  validates :template_text, length: {minimum: 5}
+
 end
